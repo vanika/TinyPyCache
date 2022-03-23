bbf89769dc0d1d0d533e8f702c375affa3670cc5
René Bwanika <renebwanika@Renes-MacBook-Air.local>
2022-03-23 21:54:45 +0100

    Implemented message protocol on client side and updated the doc.

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # TinyPyCache
 
-TinyPyCache is a Python server which is used to expose a tinyLFU in-memory cache and operations over it to networked clients. It uses a simple ASCII protocol which is human readable.
+TinyPyCache is a Python server which is used to expose a tinyLFU 
+in-memory cache and operations over it to networked clients. 
+It uses a simple ASCII protocol which is human readable.
 
-Window TinyLFU (W-TinyLFU) uses the sketch as a filter, admitting a new entry if it has a higher frequency than the entry that would have to be evicted to make room for it. Instead of filtering immediately, an admission window gives an entry a chance to build up its popularity. This avoids consecutive misses, especially in cases like sparse bursts where an entry may not be deemed suitable for long-term retention. To keep the history fresh an aging process is performed periodically or incrementally to halve all of the counters.
+Window TinyLFU (W-TinyLFU) uses the sketch as a filter, 
+admitting a new entry if it has a higher frequency than 
+the entry that would have to be evicted to make room for it. 
+Instead of filtering immediately, an admission window gives an entry 
+a chance to build up its popularity. This avoids consecutive misses, especially in cases
+like sparse bursts where an entry may not be deemed suitable for long-term retention. 
+To keep the history fresh an aging process is performed periodically or incrementally 
+to halve all of the counters.
+
+## Network protocol
+
+### Get 
+
+```
+GET <<key>> 
+```
+
+### Add
+
+```
+GET <<key>> && <<value>>
+```
--- client.py
+++ client.py
@@ -2,10 +2,18 @@ import socket
 import threading
 import time
 
+from enum import Enum
+
+
+class OperationType(Enum):
+    GET = 1
+    ADD = 2
+
 
 class TinyLFUClient:
 
     def __init__(self, port=5555, ip=socket.gethostbyname(socket.gethostname())):
+
         self.port = port
         self.ip = ip
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,16 +27,18 @@ class TinyLFUClient:
             print(f"[CONNECTED] Client connected to server at {self.ip}:{self.port}")
 
             ping_thread = threading.Thread(target=self.__ping_server, args=(self.client,))
-            thread = threading.Thread(target=self.__handle_server_connection, args=(self.client,))
+            listening_thread = threading.Thread(target=self.__handle_server_connection, args=(self.client,))
             ping_thread.start()
-            thread.start()
         except socket.error as exc:
             pass
         except KeyboardInterrupt as exc:
             pass
         finally:
-            self.client.close()
-            print("Closing the client.")
+            try:
+                self.client.close()
+                print("Closing the client.")
+            except socket.error as exc:
+                pass
 
     def __ping_server(self, connection):
 
@@ -44,28 +54,52 @@ class TinyLFUClient:
         except socket.error as exc:
             pass
         finally:
-            print(f"[DISCONNECTION] {self.port} disconnected.")
-            connection.close()
+            try:
+                print(f"[DISCONNECTION] {self.port} disconnected.")
+                connection.close()
+            except socket.error as exc:
+                pass
 
     def __handle_server_connection(self, connection):
 
         try:
-            connected = True
-            while connected:
-
-                msg = "send message every 3 seconds"
-                time.sleep(3)
-                connection.send(msg)
-
-                if msg == "disconnect":
-                    connected = False
-                else:
-                    msg = connection.recv(self.size)
-                    print(f"[SERVER] {msg}")
+            msg = connection.recv(self.size)
+            print(f"[SERVER] {msg}")
         except socket.error as exc:
             pass
         finally:
-            connection.close()
+            try:
+                connection.close()
+                print("Closing connection")
+            except socket.error as exc:
+                pass
+
+    def get(self, key):
+
+        return self.send_message(OperationType.GET, key)
+
+    def add(self, key, value):
+
+        return self.send_message(OperationType.ADD, key, value)
+
+    def send_message(self, *args):
+
+        try:
+            if self.client:
+                if len(args) == 2 and args[0] == OperationType.GET:
+                    msg = f"GET {args[1]}"
+                    self.client.send(msg)
+                    return 0
+                if len(args) == 3 and args[0] == OperationType.ADD:
+                    msg = f"ADD {args[1]} && {args[2]}"
+                    self.client.send(msg)
+                    return 0
+                return -1
+            else:
+                return -1
+        except socket.error as exc:
+            print("Error occurred. Unable to send message.")
+            return -1
 
 
 if __name__ == "__main__":
